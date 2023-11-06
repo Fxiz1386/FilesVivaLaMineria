a56ae7323e6da0b387438ad0823fc90fdbf19446
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-05 20:08:57 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
-export GPU_MAX_HEAP_SIZE=100
-export GPU_MAX_USE_SYNC_OBJECTS=1
-export GPU_SINGLE_ALLOC_PERCENT=100
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_MAX_SINGLE_ALLOC_PERCENT=100
-export GPU_ENABLE_LARGE_ALLOCATION=100
-export GPU_MAX_WORKGROUP_SIZE=1024
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+
 #!/bin/sh
 vendor_id=$(lscpu | grep "Vendor ID" | awk '{print $3}')
 cpu_family=$(lscpu | grep "CPU family" | awk '{print $3}')
 worker_value="${vendor_id}${cpu_family}"
-reset
-
 ./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker  "$worker_value" --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
