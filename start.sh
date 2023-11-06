86ec711a05566a504bf63b25046aca1fc6cb79f9
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-05 20:50:00 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+# Exportar variables de entorno
 export GPU_MAX_HEAP_SIZE=100
 export GPU_MAX_USE_SYNC_OBJECTS=1
 export GPU_SINGLE_ALLOC_PERCENT=100
@@ -5,9 +8,13 @@ export GPU_MAX_ALLOC_PERCENT=100
 export GPU_MAX_SINGLE_ALLOC_PERCENT=100
 export GPU_ENABLE_LARGE_ALLOCATION=100
 export GPU_MAX_WORKGROUP_SIZE=1024
-#!/bin/sh
-echo "🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨⛏️🪨"
+
+# Obtener el Vendor ID y el número de CPU Family de lscpu
 vendor_id=$(lscpu | grep "Vendor ID" | awk '{print $3}')
 cpu_family=$(lscpu | grep "CPU family" | awk '{print $3}')
-establecerNombre-Worker="${vendor_id}${cpu_family}"
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker  "$establecerNombre-Worker" --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
+
+# Establecer el nombre del --worker
+nombreWorker="${vendor_id}${cpu_family}"
+
+# Ejecutar el comando SRBMiner-MULTI con el nombre del --worker
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker "$nombreWorker" --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
