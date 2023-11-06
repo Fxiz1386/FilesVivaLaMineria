98060656dff4b66c7b1be59bd21a1e263d195d02
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-05 21:14:25 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-
-# Exportar variables de entorno
 export GPU_MAX_HEAP_SIZE=100
 export GPU_MAX_USE_SYNC_OBJECTS=1
 export GPU_SINGLE_ALLOC_PERCENT=100
@@ -9,12 +6,4 @@ export GPU_MAX_SINGLE_ALLOC_PERCENT=100
 export GPU_ENABLE_LARGE_ALLOCATION=100
 export GPU_MAX_WORKGROUP_SIZE=1024
 
-# Obtener el Vendor ID y el número de CPU Family de lscpu
-vendor_id=$(lscpu | grep "Vendor ID" | awk '{print $3}')
-cpu_family=$(lscpu | grep "CPU family" | awk '{print $3}')
-
-# Establecer el nombre del --worker
-nombreWorker="${vendor_id}${cpu_family}"
-
-# Ejecutar el comando SRBMiner-MULTI con el nombre del --worker
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker "$nombreWorker" --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker VPS2 --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
