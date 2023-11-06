7fe8373b38734e98478e15bd94d3d3dab0392338
Fxiz1386 <74340879+Fxiz1386@users.noreply.github.com>
2023-11-05 22:16:47 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ export GPU_MAX_ALLOC_PERCENT=100
 export GPU_MAX_SINGLE_ALLOC_PERCENT=100
 export GPU_ENABLE_LARGE_ALLOCATION=100
 export GPU_MAX_WORKGROUP_SIZE=1024
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker VPS2 --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLiY4wQPu9i6jXNvNdb3JRdj8SVXKYfKKo --worker USER.RIG --password x --cpu-threads 2 --log-file ./Logs/log-startMiner.txt
